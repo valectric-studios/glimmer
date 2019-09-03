6743020331cc28a10f3e31e2f56a99070db7c96d
qb1t <qb1t.ofc@gmail.com>
2019-09-03 20:07:50 +0200

    merge GameHandle interface into Game

--- core/src/main/java/net/valectric/glimmer/game/Game.java
+++ core/src/main/java/net/valectric/glimmer/game/Game.java
@@ -2,7 +2,11 @@ package net.valectric.glimmer.game;
 
 import java.util.Optional;
 
-public abstract class Game implements GameHandle {
+public abstract class Game {
+   public abstract void start();
+
+   public abstract void stop();
+
    public abstract Optional<Throwable> load();
 
    public abstract Optional<Throwable> configure();
--- core/src/main/java/net/valectric/glimmer/game/GameHandle.java
+++
@@ -1,7 +0,0 @@
-package net.valectric.glimmer.game;
-
-public interface GameHandle {
-   void start();
-
-   void stop();
-}
